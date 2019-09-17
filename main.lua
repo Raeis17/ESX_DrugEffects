b119488abd9dd20d233724d70f3502d188a05b08
Raeis17 <55444573+Raeis17@users.noreply.github.com>
2019-09-17 13:46:11 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,228 +1,55 @@
-ESX                  = nil
-local IsAlreadyDrug = false
-local DrugLevel     = -1
+ESX = nil
 
-Citizen.CreateThread(function()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(0)
-  end
+
+TriggerEvent('esx:getSharedObject', function(obj)
+	ESX = obj
 end)
 
-AddEventHandler('esx_status:loaded', function(status)
-
-  TriggerEvent('esx_status:registerStatus', 'drug', 0, '#9ec617', 
-    function(status)
-      if status.val > 0 then
-        return true
-      else
-        return false
-      end
-    end,
-    function(status)
-      status.remove(1500)
-    end
-  )
-
-	Citizen.CreateThread(function()
-
-		while true do
-
-			Wait(1000)
-
-			TriggerEvent('esx_status:getStatus', 'drug', function(status)
-				
-				if status.val > 0 then
-							
-				    local start = true
-
-				    if IsAlreadyDrug then
-						start = false
-					end
-
-					local level = 0
-
-					if status.val <= 999999 then
-						level = 0
-					else
-						overdose()											
-					end
-
-					if level ~= DrugLevel then
-					end
-
-					IsAlreadyDrug = true
-					DrugLevel     = level
-				end
-
-				if status.val == 0 then
-				  
-					if IsAlreadyDrug then
-						Normal()
-					end
-
-					IsAlreadyDrug = false
-					DrugLevel     = -1
-
-				end
-
-			end)
-
-		end
-
-	end)
-
-end)
-
---When effects ends go back to normal
-function Normal()
-
-  Citizen.CreateThread(function()
-    
-    local playerPed = GetPlayerPed(-1)
-			
-    ClearTimecycleModifier()
-    ResetScenarioTypesEnabled()
-    ResetPedMovementClipset(playerPed, 0) --<- it might cause the push of the vehicles
-    SetPedIsDrunk(playerPed, false)
-    SetPedMotionBlur(playerPed, false)
-	
-  end)
-
-end
-
---In case too much drugs dies of overdose set everything back
-function overdose()
-
-  Citizen.CreateThread(function()
-
-    local playerPed = GetPlayerPed(-1)
-	
-    SetEntityHealth(playerPed, 0)
-    ClearTimecycleModifier()
-    ResetScenarioTypesEnabled()
-    ResetPedMovementClipset(playerPed, 0)
-    SetPedIsDrunk(playerPed, false)
-    SetPedMotionBlur(playerPed, false)
-	--TriggerClientEvent('esx_status:remove', _source, 'drug', status.val)
-
-  end)
-
-end
-
---Drugs Effects
-
---Weed
-RegisterNetEvent('esx_drugeffects:onWeed')
-AddEventHandler('esx_drugeffects:onWeed', function()
-  
-  local playerPed = GetPlayerPed(-1)
-  
-    RequestAnimSet("move_m@hipster@a") 
-    while not HasAnimSetLoaded("move_m@hipster@a") do
-      Citizen.Wait(0)
-    end    
-
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetTimecycleModifier("spectator5")
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@hipster@a", true)
-    SetPedIsDrunk(playerPed, true)
-    
-    --Efects
-    local player = GetPlayerPed(-1)
-    SetRunSprintMultiplierForPlayer(player, 1.3)
+ESX.RegisterUsableItem('weed', function(source)
         
-    Wait(300000)
+        local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	xPlayer.removeInventoryItem('weed', 1)
 
-    SetRunSprintMultiplierForPlayer(player, 1.0)		
+	TriggerClientEvent('esx_status:add', _source, 'drug', 166000)
+	TriggerClientEvent('esx_drugeffects:onWeed', source)
 end)
 
---Opium
-RegisterNetEvent('esx_drugeffects:onOpium')
-AddEventHandler('esx_drugeffects:onOpium', function()
-  
-  local playerPed = GetPlayerPed(-1)
-  
-        RequestAnimSet("move_m@drunk@moderatedrunk") 
-    while not HasAnimSetLoaded("move_m@drunk@moderatedrunk") do
-      Citizen.Wait(0)
-    end    
+ESX.RegisterUsableItem('opium', function(source)
+       
+        local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('opium', 1)
 
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetTimecycleModifier("spectator5")
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@drunk@moderatedrunk", true)
-    SetPedIsDrunk(playerPed, true)
-    
-    --Efects
-    local player = PlayerId()
-    SetRunSprintMultiplierForPlayer(player, 1.2)
-    SetSwimMultiplierForPlayer(player, 1.3)
-
-    Wait(520000)
-
-    SetRunSprintMultiplierForPlayer(player, 1.0)
-    SetSwimMultiplierForPlayer(player, 1.0)
- end)
-
---Meth
-RegisterNetEvent('esx_drugeffects:onMeth')
-AddEventHandler('esx_drugeffects:onMeth', function()
-  
-  local playerPed = GetPlayerPed(-1)
-  local maxHealth = GetEntityMaxHealth(playerPed)
-
-        RequestAnimSet("move_injured_generic") 
-    while not HasAnimSetLoaded("move_injured_generic") do
-      Citizen.Wait(0)
-    end    
-
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetTimecycleModifier("spectator5")
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_injured_generic", true)
-    SetPedIsDrunk(playerPed, true)
-    
-   --Efects
-    local player = PlayerId()  
-    local health = GetEntityHealth(playerPed)
-    local newHealth = math.min(maxHealth , math.floor(health + maxHealth/8))
-    SetEntityHealth(playerPed, newHealth)
-    
+	TriggerClientEvent('esx_status:add', _source, 'drug', 249000)
+	TriggerClientEvent('esx_drugeffects:onOpium', source)
 end)
 
---Coke
-RegisterNetEvent('esx_drugeffects:onCoke')
-AddEventHandler('esx_drugeffects:onCoke', function()
-  
-  local playerPed = GetPlayerPed(-1)
-  local maxHealth = GetEntityMaxHealth(playerPed)
+ESX.RegisterUsableItem('meth', function(source)
+        
+        local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('meth', 1)
 
-        RequestAnimSet("move_m@hurry_butch@a") 
-    while not HasAnimSetLoaded("move_m@hurry_butch@a") do
-      Citizen.Wait(0)
-    end    
+	TriggerClientEvent('esx_status:add', _source, 'drug', 333000)
+	TriggerClientEvent('esx_drugeffects:onMeth', source)
+end)
 
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetTimecycleModifier("spectator5")
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@hurry_butch@a", true)
-    SetPedIsDrunk(playerPed, true)
-    
-    --Efects
-    local player = PlayerId()
-    AddArmourToPed(playerPed, 100)
-    local health = GetEntityHealth(playerPed)
-    local newHealth = math.min(maxHealth , math.floor(health + maxHealth/6))
-    SetEntityHealth(playerPed, newHealth)
-    
+ESX.RegisterUsableItem('coke', function(source)
+        
+        local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('coke', 1)
+
+	TriggerClientEvent('esx_status:add', _source, 'drug', 499000)
+	TriggerClientEvent('esx_drugeffects:onCoke', source)
+end)
+
+ESX.RegisterUsableItem('xanax', function(source)
+        
+        local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('xanax', 1)
+
+	TriggerClientEvent('esx_status:remove', _source, 'drug', 249000)
 end)
